16e7d14728edf829eace8b49fb70c901573b6476
Roshan Rengadurai <159981741+Roshan-Rengadurai@users.noreply.github.com>
2024-10-19 19:00:13 -0400

    Updated "Half-Baked Website" to see how it looks on github

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "custom-blue": "#3490DC",
-        "custom-green": "#2ECC71",
-        "custom-orange": "#E67E22",
-        "custom-purple": "#9B59B6",
-        "custom-pink": "#E74C3C",
-        "custom-yellow": "#F1C40F",
+        
       },
       fontFamily: {
         sans: ["Inter", "sans-serif"],

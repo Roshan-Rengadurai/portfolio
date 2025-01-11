c2c5ead9a686e623dc4094c74a91b5d0f72c3200
Roshan-Rengadurai <roshan.rengadurai@gmail.com>
2025-01-11 18:33:58 -0500

    updated linkedin url

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -52,7 +52,7 @@ export const DATA = {
       },
       LinkedIn: {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/roshan-rengadurai-22601a2b2/",
+        url: "https://www.linkedin.com/in/roshan-rengadurai/",
         icon: Icons.linkedin,
 
         navbar: true,
